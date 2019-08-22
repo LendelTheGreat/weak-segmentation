fd517517154b5eab921e81293b254c3414afd63f
LendelTheGreat <langer.tomas@yahoo.com>
2019-08-22 16:08:42 +0000

    update data paths and debug preprocessing script

--- data/dataset_split.py
+++ data/dataset_split.py
@@ -1,15 +1,15 @@
 all_ids = list(range(33))
-train_segmap_ids = [0, 1, 2, 17]
-train_class_ids = [5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32]
+train_strong_ids = [0, 1, 2, 17]
+train_weak_ids = [5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32]
 val_ids = [3, 26, 14, 18]
 test_ids = [4, 27, 20, 19]
 
-assert sorted(train_segmap_ids + train_class_ids + val_ids + test_ids) == all_ids, 'Data split is not correct!'
+assert sorted(train_strong_ids + train_weak_ids + val_ids + test_ids) == all_ids, 'Data split is not correct!'
 
 if __name__== "__main__":
     print('all_ids {} = {:.2%}'.format(len(all_ids), len(all_ids) / len(all_ids)))
-    print('train_segmap_ids {} = {:.2%}'.format(len(train_segmap_ids), len(train_segmap_ids) / len(all_ids)))
-    print('train_class_ids {} = {:.2%}'.format(len(train_class_ids), len(train_class_ids) / len(all_ids)))
+    print('train_strong_ids {} = {:.2%}'.format(len(train_strong_ids), len(train_strong_ids) / len(all_ids)))
+    print('train_weak_ids {} = {:.2%}'.format(len(train_weak_ids), len(train_weak_ids) / len(all_ids)))
     print('val_ids {} = {:.2%}'.format(len(val_ids), len(val_ids) / len(all_ids)))
     print('test_ids {} = {:.2%}'.format(len(test_ids), len(test_ids) / len(all_ids)))
 
--- data/preprocess.py
+++ data/preprocess.py
@@ -3,12 +3,30 @@ import os
 import numpy as np
 from skimage import io
 
-from data.dataset_split import train_segmap_ids, train_class_ids, val_ids, test_ids
+print(os.getcwd())
+from data.dataset_split import train_strong_ids, train_weak_ids, val_ids, test_ids
 from utils.utils import segmap_to_class_vec, image_to_segmap
 
-dir_images = '~data/raw/top'
-dir_segmaps = '~data/raw/gts'
-dir_output = '~data/crops'
+dir_images = os.path.join(os.getenv('HOME'), 'data/raw/top/')
+dir_segmaps = os.path.join(os.getenv('HOME'), 'data/raw/gts/')
+dir_output = os.path.join(os.getenv('HOME'), 'data/crops/')
+
+dir_train_strong_img = os.path.join(dir_output, 'train_strong', 'img')
+dir_train_strong_seg = os.path.join(dir_output, 'train_strong', 'seg')
+dir_train_weak_img = os.path.join(dir_output, 'train_weak', 'img')
+dir_train_weak_seg = os.path.join(dir_output, 'train_weak', 'seg')
+dir_val_img = os.path.join(dir_output, 'val', 'img')
+dir_val_seg = os.path.join(dir_output, 'val', 'seg')
+dir_test_img = os.path.join(dir_output, 'test', 'img')
+dir_test_seg = os.path.join(dir_output, 'test', 'seg')
+os.makedirs(dir_train_strong_img, exist_ok=True)
+os.makedirs(dir_train_strong_seg, exist_ok=True)
+os.makedirs(dir_train_weak_img, exist_ok=True)
+os.makedirs(dir_train_weak_seg, exist_ok=True)
+os.makedirs(dir_val_img, exist_ok=True)
+os.makedirs(dir_val_seg, exist_ok=True)
+os.makedirs(dir_test_img, exist_ok=True)
+os.makedirs(dir_test_seg, exist_ok=True)
 
 crop_size = 192
 crop_halfsize = int(crop_size / 2)
@@ -26,6 +44,7 @@ def run():
     total_class_sums = np.zeros(5)
     total_n_crops = 0
     for i in range(len(images)):
+        print('Cropping image {} / {}'.format(i, len(images)))
         class_sums = np.zeros(5)
         n_crops = 0
         
@@ -43,18 +62,26 @@ def run():
                 segmap_crop = segmap[y-crop_halfsize:y+crop_halfsize, x-crop_halfsize:x+crop_halfsize, :]
                 
                 #Save image_crop and segmap_crop
-                if i in train_class_ids:
-                    io.imsave(os.path.join(dir_output, 'train_class', 'img_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x), image_crop)
-                    io.imsave(os.path.join(dir_output, 'train_class', 'seg_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x)), segmap_crop)
-                elif i in train_segmap_ids:
-                    io.imsave(os.path.join(dir_output, 'train_segmap', 'img_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x), image_crop)
-                    io.imsave(os.path.join(dir_output, 'train_segmap', 'seg_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x)), segmap_crop)
+                if i in train_strong_ids:
+                    io.imsave(os.path.join(dir_train_strong_img, 'img_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              image_crop, check_contrast=False)
+                    io.imsave(os.path.join(dir_train_strong_seg, 'seg_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              segmap_crop, check_contrast=False)
+                elif i in train_weak_ids:
+                    io.imsave(os.path.join(dir_train_weak_img, 'img_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              image_crop, check_contrast=False)
+                    io.imsave(os.path.join(dir_train_weak_seg, 'seg_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              segmap_crop, check_contrast=False)
                 elif i in val_ids:
-                    io.imsave(os.path.join(dir_output, 'val', 'img_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x), image_crop)
-                    io.imsave(os.path.join(dir_output, 'val', 'seg_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x)), segmap_crop)
+                    io.imsave(os.path.join(dir_val_img, 'img_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              image_crop, check_contrast=False)
+                    io.imsave(os.path.join(dir_val_seg, 'seg_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              segmap_crop, check_contrast=False)
                 elif i in test_ids:
-                    io.imsave(os.path.join(dir_output, 'test', 'img_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x), image_crop)
-                    io.imsave(os.path.join(dir_output, 'test', 'seg_{:0>2d}_{:0>4d}_{:0>4d}.tif'.format(i, y, x)), segmap_crop)
+                    io.imsave(os.path.join(dir_test_img, 'img_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              image_crop, check_contrast=False)
+                    io.imsave(os.path.join(dir_test_seg, 'seg_{:0>2d}_{:0>4d}_{:0>4d}.png'.format(i, y, x)),
+                              segmap_crop, check_contrast=False)
                 
                 class_vec = segmap_to_class_vec(image_to_segmap(segmap_crop), class_presence_threshold=160)
                 class_sums += class_vec
@@ -64,7 +91,11 @@ def run():
         total_n_crops += n_crops
         
     print('Total class frequencies: {}'.format(total_class_sums / total_n_crops))
-    print('Saved {} crops into {}'.format(n_crops, dir_output))
+    print('Saved {} crops into {}'.format(total_n_crops, dir_output))
     
 if __name__== "__main__":
   run()
+
+# Last output of this script:
+# Total class frequencies: [0.81808962 0.77574951 0.81352602 0.81644166 0.29758509]
+# Saved 15777 crops into /home/ubuntu/data/crops/
